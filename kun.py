bdd56619a077d70352cfc7b94eb5b5fa96977272
mcanallkf <kanallduaempat@gmail.com>
2026-05-22 21:10:01 +0700

    Update kun.py

--- kun.py
+++ kun.py
@@ -22,6 +22,7 @@ import socks
 attemps = 0
 os.system("clear")
 print("""
+╭───╮
 \033[33m                       ╭───╮
 \033[95m╭───╮    ╭───╮\033[33m         │   │
 \033[95m│   │╭──╮│   │\033[36m─────────╮\033[33m   ╰───────╮
@@ -32,9 +33,9 @@ print("""
 \033[95m│     ╭─╮    │\033[36m ╰───╯   │\033[33m  ╰────╯   │ 
 \033[95m╰─────╯ ╰────╯\033[36m─────────╯\033[33m───────────╯""")
 print(f"\033[97m╔{'═' * 71}╗")
-print(f"\033[97m║\033[104m{' ' * 21}Don't attack government sites{' ' * 21}\033[0m║")
-print(f"\033[97m║\033[104m{' ' * 20}Just to fight to help Palestine{' ' * 20}\033[0m║")
-print(f"\033[97m║\033[104m{' ' * 28}_Use it wisely_{' ' * 28}\033[0m║")
+print(f"\033[97m║\033[104m{' ' * 21}SCRIPT KHUSUS ATTACK ZI0NIST{' ' * 21}\033[0m║")
+print(f"\033[97m║\033[104m{' ' * 19}TIDAK UNTUK WEBSITE PEMERINTAH {' ' * 19}\033[0m║")
+print(f"\033[97m║\033[104m{' ' * 28}_BISI DORAKA MOYANG_{' ' * 28}\033[0m║")
 print(f"\033[97m╚{'═' * 71}╝")
 while attemps < 100:
     print("\033[38;5;6m┏━━KunFayz━━⬣")
